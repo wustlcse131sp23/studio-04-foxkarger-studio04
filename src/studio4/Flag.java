ae7349105193099ac6b645ea6c067e6e50d43b87
noam <noam@brandons-mbp.localdomain>
2023-02-23 12:22:41 -0600

    Skeet

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.filledRectangle(.5, .5, .5, .375);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(.5,.5,.3);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(.4,.6,.03);
+		StdDraw.filledCircle(.6,.6,.03);
+		StdDraw.filledEllipse(.5, .4, .09,.125);
 	}
 }
